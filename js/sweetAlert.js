d8a09f4c17efd84fada7b16b1c1e4f21fe340da8
pulpifrutas22 <infopulpifrutas@gmail.com>
2022-10-05 17:41:22 -0500

    nuevo html y alerta

---
+++ js/sweetAlert.js
@@ -0,0 +1,60 @@
+Swal.fire({
+    title: 'Bienvenido a PulpiFrutas',
+    text: 'Esta página web aún no esta en funcionamiento, por lo cual la página es una demostración.',
+    // html:
+	icon: 'info',
+	// confirmButtonText:
+	footer: '<span class="rojo">¡Esta información es importante!</span>',
+	// width: '25%',
+	padding: '1rem',
+	// background:
+	// grow: 
+	backdrop: 'true',
+	// timer: '5000',
+	// timerProgressBar: 'true',
+	// toast: 'true',
+	position: 'center',
+	allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	stopKeydownPropagation: false,
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass: 
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
